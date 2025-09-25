41c971e8223a5c5f675c31d3d233263c9b0c8eab
Hussein Khrayzat <hkhrayza@c2r4s5.42beirut.com>
2025-09-25 18:23:54 +0300

    deleted everything

--- Client/src/game/config/constants.ts
+++
@@ -1,4 +0,0 @@
-import type { ObstacleShape } from "../../types";
-
-export const SHAPES: ObstacleShape[] = ["sphere", "cylinder", "cone", "capsule", "disc", "box"];
-export const SHAPE_WEIGHTS: number[] = [3, 2, 2, 1, 2, 2]; // sphere more common, capsule rarer
